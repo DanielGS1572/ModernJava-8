f8b009773191b0ef3de223828b65ff3b4af0a3de
DanielGS1572 <gonzalisd@gmail.com>
2024-11-12 22:11:12 -0600

    modern java

---
+++ java-8/src/com/learnJava/functionalInterfaces/A_ConsumerExample.java
@@ -0,0 +1,85 @@
+package com.learnJava.functionalInterfaces;
+
+import com.learnJava.data.Student;
+import com.learnJava.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class A_ConsumerExample {
+/*
+En esta clase el metodo main esta hasta abajo
+
+Se usan dos clases para los datos
+- Student
+- StudentDatabase.- Regresa una lista de Student
+
+Primary interfaces      -       Extensions
+
+Consumer                -       BiConsumer
+=> Accept some input and do some operation over that input
+
+Predicate               -       BiPredicate
+Function                -       BiFunction, UnaryOperator, BinaryOperator
+Supplier
+*/
+    static Consumer<Student>  c1= p -> System.out.println(p);
+
+    static Consumer<Student>  c2= p -> System.out.print(p.getName().toUpperCase());
+
+    static Consumer<Student>  c3= p -> System.out.println(p.getActivities());
+
+
+    public static void printName(){
+
+        List<Student> personList = StudentDataBase.getAllStudents();
+
+        personList.forEach(c1);         //Ver que aqui se da la programación funcional
+        // Dar click en forEach para ver que recibe como parametro
+        // Ver que recibe un consumer y de esta manera se puede ejecutar diferentes implementaciones
+        System.out.println();
+        System.out.println();
+
+
+    }
+
+    public static void printNameAndActivities(){
+        System.out.println("printNameAndActivities : ");
+        List<Student> personList = StudentDataBase.getAllStudents();
+        personList.forEach(c2.andThen(c3));     //Consumer Chaining.- Primero ejecutará c2 y luego c3
+        //ver que andThen pertenece a la clase consumer que solo recibirá un tipo de dato que sea/herede del objeto que esta siendo llamado el metodo .andThen
+        //en este caso debe ser de tipo Student
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void printNameAndActivitiesUsingCondition(){
+        System.out.println("printNameAndActivitiesUsingCondition : ");
+        List<Student> personList = StudentDataBase.getAllStudents();
+        personList.forEach((s) -> {
+            if( s.getGradeLevel()>=3 && s.getGpa()>3.9){
+                c2.andThen(c3).accept(s);           //ver que se tiene que usar accept para una implementación al vuelo
+            }
+        });
+        System.out.println();
+        System.out.println();
+
+    }
+
+    public static void main(String[] args) {
+
+        Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
+        //Ver que accept es para que se ejecute la implementación de la interfaz que
+        // acepta un valor que solo será consumido es decir, no regresará nada
+        c1.accept("java8");
+
+        printName();
+        printNameAndActivities();
+        printNameAndActivitiesUsingCondition();
+
+
+
+
+
+    }
+}
--- java-8/src/com/learnJava/functionalInterfaces/B_BiConsumerExample.java
+++ java-8/src/com/learnJava/functionalInterfaces/B_BiConsumerExample.java
@@ -7,23 +7,28 @@ import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
-public class BiConsumerExample {
+public class B_BiConsumerExample {
+    /*
+    - BiConsumer acepta dos inputs (para revisar simplemente dar click en la interfaz
+    y ver que tiene dos genericos distintos
+    */
 
     public static void nameAndActivities(){
-
+//misma implementación que el Caso de consumer, pero ver que aqui se tiene mas control al poder concatenar con otro string (ambos parametro)
         BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name + " : " + activities);
 
         Consumer<String> stringConsumer = (name) -> System.out.println("name is  :" + name);
 
         List<Student> students = StudentDataBase.getAllStudents();
 
-        students.forEach((s) -> studentBiConsumer.accept(s.getName(),s.getActivities()));
+        students.forEach((s) -> studentBiConsumer.accept(s.getName(),s.getActivities()));   //Ver que como se esta haciendo la implementación del lambda ( s -> ) se tiene que usar accept
     }
 
     public static void main(String[] args) {
 
-
+//ver que se definen los tipos dentro del operador diamante
         BiConsumer<String, String> biConsumer = (a,b) -> {
+            //dado que Consumer no regresa información por eso nada más se usa un sout
             System.out.println(" a : "  +  a + " b : " + b );
         };
         biConsumer.accept("java7" , "java8");
@@ -42,7 +47,7 @@ public class BiConsumerExample {
         };
 
 
-        multiply.andThen(addition).andThen(division).accept(10,5);
+        multiply.andThen(addition).andThen(division).accept(10,5); //TODO importante... ver que el accept es como pasarle los dos parametros al biFunction por cada andThen (junto con el primer multiply)
 
 
         nameAndActivities();

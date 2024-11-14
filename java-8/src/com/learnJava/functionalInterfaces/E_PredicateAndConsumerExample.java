7e7584302d1c6b98eeb347781231111de9fb96af
Daniel <gonzalisd@gmail.com>
2024-11-14 09:23:29 -0600

    java8

--- java-8/src/com/learnJava/functionalInterfaces/E_PredicateAndConsumerExample.java
+++ java-8/src/com/learnJava/functionalInterfaces/E_PredicateAndConsumerExample.java
@@ -8,39 +8,32 @@ import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-public class PredicateAndConsumerExample {
+public class E_PredicateAndConsumerExample {
 
     static Predicate<Student> p1 = (s) -> s.getGradeLevel()>=3;
 
     static Predicate<Student> p2 = (s) -> s.getGpa()>=3.9;
-
+//Ver que puede ser static o no (dos lineas pasadas + linea siguiente)
     BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name + " : " + activities);
 
     Consumer<Student> studentConsumer = (student) -> {
 
-        if(p1.and(p2).test(student)){
+        if(p1.and(p2).test(student)){  //Predicate para GradeLevel y GPA
             studentBiConsumer.accept(student.getName(),student.getActivities());
         }
     };
 
-    Consumer<Student> studentConsumer1 = ((student) -> {
-
-        if(p1.and(p2).test(student)){
-            studentBiConsumer.accept(student.getName(),student.getActivities());
-        }
-    });
 
     public void printNameandActivities(List<Student> studentList){
-
-        studentList.forEach(studentConsumer);
-
+        studentList.forEach(studentConsumer);               //recordar que el forEach recibe un consumer e internamente hace el .accept
+                                                            //ver que ademas dentro del consumer utiliza predicta y un biConsumer
     }
 
     public static void main(String[] args) {
 
         List<Student> studentList = StudentDataBase.getAllStudents();
 
-        new PredicateAndConsumerExample().printNameandActivities(studentList);
+        new E_PredicateAndConsumerExample().printNameandActivities(studentList);
 
     }
 }

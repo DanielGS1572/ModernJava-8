65c31f6791ba387a6f1aa26d0fc0e13e0a012cd8
Daniel <gonzalisd@gmail.com>
2024-11-15 18:28:37 -0600

    java8

--- java-8/src/com/learnJava/AKatas/Kata.java
+++ java-8/src/com/learnJava/AKatas/Kata.java
@@ -6,21 +6,18 @@ import com.learnJava.data.StudentDataBase;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Kata {
-    static Consumer<String> c1 = (s)-> System.out.println(s.toUpperCase());
-    static Consumer<Student> c2 = student -> System.out.println(student);
-    static Consumer<Student> c3 = student -> System.out.print(student.getName().toUpperCase());
-    static Consumer<Student> c4 = student -> System.out.println(student.getActivities());
+    static Predicate<Integer> mod2 = i -> i%2 == 0;
+    static Predicate<Integer> mod5 = i -> i%5 == 0;
     public static void main(String[] args){
-
-        StudentDataBase.getAllStudents().forEach(s->{
-            if(s.getGradeLevel() ==4 && s.getGpa()==3.9){
-                c3.andThen(c2).accept(s);
-            }
-        });
+        System.out.println(mod2.and(mod5).test(4));
+        System.out.println(mod5.negate().and(mod2).test(4));
     }
+
 }

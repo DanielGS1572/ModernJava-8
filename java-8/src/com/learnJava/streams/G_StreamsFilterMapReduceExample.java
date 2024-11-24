bf2bab3574682eb14fcc29f02c4af700cd496939
DanielGS1572 <gonzalisd@gmail.com>
2024-11-24 15:11:36 -0600

    modern java

--- java-8/src/com/learnJava/streams/G_StreamsFilterMapReduceExample.java
+++ java-8/src/com/learnJava/streams/G_StreamsFilterMapReduceExample.java
@@ -5,7 +5,7 @@ import com.learnJava.data.StudentDataBase;
 
 import java.util.function.Predicate;
 
-public class StreamsFilterMapReduceExample {
+public class G_StreamsFilterMapReduceExample {
 
     static Predicate<Student> genderPredicte =(student -> {
         return student.getGender().equals("male");
@@ -14,12 +14,14 @@ public class StreamsFilterMapReduceExample {
     static Predicate<Student> gradeLevel =(student -> {
         return student.getGradeLevel()==2.0;
     });
-
+/*
+        Lo que se obtendrá es el numero total de notebooks de la colección
+*/
     private static int noOfNoteBooks(){
 
         int totalNoOfnoteBooks = StudentDataBase.getAllStudents().stream()
                 //.filter(genderPredicte)
-                .filter(gradeLevel)
+               // .filter(gradeLevel)
               /*  .peek(s->{
                     System.out.println("s : "+ s);
                 })*/
@@ -27,7 +29,7 @@ public class StreamsFilterMapReduceExample {
                /* .peek(s1->{
                     System.out.println("s1 : "+ s1);
                 })*/
-               // .reduce(0,(a,b)->a+b); //summing the notebooks.
+               // .reduce(0,(a,b)->a+b); //summing the notebooks.       //Todo importante... Integer no tiene la opción de multiplicación por lo que la alternativa es desglozar la operación
                 .reduce(0,Integer::sum); //summing the notebooks.
         return  totalNoOfnoteBooks;
 
